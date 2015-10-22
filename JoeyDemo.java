35adddea7739c7cf300c71848ac2ce33f97e4bc6
Joe Richter <joerichterdev@gmail.com>
2015-10-21 18:52:50 -0700

    deleting file due to error

--- JoeyDemo.java
+++
@@ -1,8 +0,0 @@
-// This is a simple class to test and demo creating a new branch,
-// making changes and then merging those changes back with the master branch
-
-public class JoeyDemo {
-   public static void main(String[] args) {
-      System.out.println("I am making changes to the testBranch");
-   }
-}
